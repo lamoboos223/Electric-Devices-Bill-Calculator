fff20dd6a515205085513818cafd64d3259ea776
Lama Alosaimi <lalosaimi@tahakom.com>
2020-11-29 20:12:07 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'electricity-calculator';
+
+  public watt: number;
+  public hours: number;
+  public days: number;
+  public result: number;
+
+  calculate(){
+    let wattPerHour = (this.watt * this.hours)/1000
+    let bill = 0;
+    if(wattPerHour <= 6000)
+      bill = 18
+    else
+      bill = 30;
+    this.result = ((wattPerHour* this.days) * bill);
+  }
 }
